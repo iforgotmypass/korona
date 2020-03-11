834b5ad5d913b94bf6128c2cdffd78fff4cd71d1
iforgotmypass <39384751+iforgotmypass@users.noreply.github.com>
2020-03-12 00:18:15 +0100

    Update korona.py

--- korona.py
+++ korona.py
@@ -2,20 +2,28 @@ import requests
 import json
 import csv
 import datetime
+import schedule
+import time
 from bs4 import BeautifulSoup
-dzis = datetime.datetime.now().strftime("%d-%m-%y")
-strona = requests.get('https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2')
-soup = BeautifulSoup(strona.content, 'html.parser')
 
-lista = json.loads(soup.find(id='registerData').get_text())
+def sprawdz():
+    dzis = datetime.datetime.now().strftime('%d-%m-%y')
+    strona = requests.get('https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2')
+    soup = BeautifulSoup(strona.content, 'html.parser')
+    lista = json.loads(soup.find(id='registerData').get_text())
+    listacsv = csv.reader(lista['data'].split("\n"), delimiter=';')
+    with open(f'{dzis}.csv',mode='w') as dzisiaj:
+        dzisiaj = csv.writer(dzisiaj, delimiter=",")
+        for x in listacsv:
+            try:
+                x.pop()
+            except:
+                print('Lista pusta')
+            dzisiaj.writerow(x)
+    print(f'Aktualizacja godz. 20:00 {dzis}')
 
-listacsv = csv.reader(lista['data'].split("\n"), delimiter=';')
+schedule.every().day.at('20:00').do(sprawdz)
 
-with open(f'{dzis}.csv',mode='w') as dzisiaj:
-    dzisiaj = csv.writer(dzisiaj, delimiter=",")
-    for x in listacsv:
-        try:
-            x.pop()
-        except:
-            print("Lista pusta")
-        dzisiaj.writerow(x)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
